dae1ff837db1e4268271bdfd6468863a63e62941
adham <adhamhamdy682@gmail.com>
2024-06-10 19:34:58 +0300

    tourist profile

--- lib/screens/touristview/profile_pages/profile_settings_screen.dart
+++ lib/screens/touristview/profile_pages/profile_settings_screen.dart
@@ -1,20 +1,27 @@
 import 'dart:io';
-
-import 'package:final_project/components/custom_text.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfileSettingsScreen extends StatefulWidget {
-  const ProfileSettingsScreen({super.key});
+  const ProfileSettingsScreen({Key? key}) : super(key: key);
 
   @override
-  State<ProfileSettingsScreen> createState() => _PickImageState();
+  State<ProfileSettingsScreen> createState() => _ProfileSettingsScreenState();
 }
 
-class _PickImageState extends State<ProfileSettingsScreen> {
+class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
   Uint8List? _image;
-  File? selectedIMage;
+  File? selectedImage;
+  bool isEditingName = false;
+  bool isEditingPassword = false;
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +41,7 @@ class _PickImageState extends State<ProfileSettingsScreen> {
             ),
           ),
         ),
-        title: TextForTitleL(data: 'profile settings'),
+        title: Text('Profile Settings'),
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 27),
@@ -44,158 +51,213 @@ class _PickImageState extends State<ProfileSettingsScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(
-                height: 66,
-              ),
+              const SizedBox(height: 66),
               Center(
                 child: Stack(
                   children: [
                     _image != null
-                        ? CircleAvatar(
-                            radius: 65, backgroundImage: MemoryImage(_image!))
+                        ? CircleAvatar(radius: 65, backgroundImage: MemoryImage(_image!))
                         : const CircleAvatar(
                             radius: 65,
-                            backgroundImage: AssetImage(
-                                'assets/images/Dave-Dickinson-Headshot.jpg'),
+                            backgroundImage: AssetImage('assets/images/Dave-Dickinson-Headshot.jpg'),
                           ),
                     Positioned(
-                        bottom: -10,
-                        left: 85,
-                        child: IconButton(
-                            onPressed: () {
-                              showImagePickerOption(context);
-                            },
-                            icon: const Icon(Icons.add_a_photo)))
+                      bottom: -10,
+                      left: 85,
+                      child: IconButton(
+                        onPressed: () {
+                          showImagePickerOption(context);
+                        },
+                        icon: const Icon(Icons.add_a_photo),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 24,
-              ),
+              const SizedBox(height: 24),
               Text(
                 'Name',
                 style: TextStyle(
-                    fontFamily: 'Inter',
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15),
+                  fontFamily: 'Inter',
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                ),
               ),
               TextField(
+                controller: nameController,
+                enabled: isEditingName,
+                style: TextStyle(color: Theme.of(context).primaryColor),
                 decoration: InputDecoration(
                   hintText: 'User Name',
                   hintStyle: const TextStyle(color: Color(0XFFD8D8D8)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
                   focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0XFFF5903F),
-                      )),
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
                   enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0XFFD8D8D8),
-                      )),
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 27,
-              ),
+              const SizedBox(height: 27),
               Center(
                 child: MaterialButton(
-                  //minWidth: MediaQuery.of(context).size.width,
                   height: 42,
                   minWidth: 233,
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      isEditingName = !isEditingName;
+                    });
+                  },
                   shape: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(50),
                     borderSide: BorderSide.none,
                   ),
                   color: const Color(0XFFF5903F),
-                  child: const Text(
-                    'Edit Name',
-                    style: TextStyle(
+                  child: Text(
+                    isEditingName ? 'Done' : 'Edit Name',
+                    style: const TextStyle(
                       color: Colors.white,
                       fontSize: 20,
                     ),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 27,
-              ),
+              const SizedBox(height: 27),
               Text(
                 'Password',
                 style: TextStyle(
-                    fontFamily: 'Inter',
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'enter your password',
-                  hintStyle: const TextStyle(color: Color(0XFFD8D8D8)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0XFFF5903F),
-                      )),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0XFFD8D8D8),
-                      )),
+                  fontFamily: 'Inter',
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
                 ),
               ),
-              const SizedBox(
-                height: 9,
+              TextField(
+                controller: passwordController,
+                enabled: isEditingPassword,
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  hintStyle: const TextStyle(color: Color(0XFFD8D8D8)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
               ),
+              const SizedBox(height: 9),
               Text(
                 'Confirm Password',
                 style: TextStyle(
-                    fontFamily: 'Inter',
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15),
+                  fontFamily: 'Inter',
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                ),
               ),
               TextField(
+                controller: confirmPasswordController,
+                enabled: isEditingPassword,
+                obscureText: !_isConfirmPasswordVisible,
                 decoration: InputDecoration(
                   hintText: 'Re-enter your password',
                   hintStyle: const TextStyle(color: Color(0XFFD8D8D8)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
                   focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0XFFF5903F),
-                      )),
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
                   enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0XFFD8D8D8),
-                      )),
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0XFFF5903F),
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                      });
+                    },
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 27,
-              ),
+              const SizedBox(height: 27),
               Center(
                 child: MaterialButton(
-                  //minWidth: MediaQuery.of(context).size.width,
                   height: 42,
                   minWidth: 233,
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      isEditingPassword = !isEditingPassword;
+                    });
+                  },
                   shape: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(50),
                     borderSide: BorderSide.none,
                   ),
                   color: const Color(0XFFF5903F),
-                  child: const Text(
-                    'Edit Password',
-                    style: TextStyle(
+                                    child: Text(
+                    isEditingPassword ? 'Save Password' : 'Edit Password',
+                    style: const TextStyle(
                       color: Colors.white,
                       fontSize: 20,
                     ),
@@ -211,80 +273,78 @@ class _PickImageState extends State<ProfileSettingsScreen> {
 
   void showImagePickerOption(BuildContext context) {
     showModalBottomSheet(
-        backgroundColor: Colors.white,
-        context: context,
-        builder: (builder) {
-          return Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 4.5,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: InkWell(
-                      onTap: () {
-                        _pickImageFromGallery();
-                      },
-                      child: const SizedBox(
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.image,
-                              size: 70,
-                            ),
-                            Text("Gallery")
-                          ],
-                        ),
+      backgroundColor: Colors.white,
+      context: context,
+      builder: (builder) {
+        return Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 4.5,
+            child: Row(
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      _pickImageFromGallery();
+                    },
+                    child: const SizedBox(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.image,
+                            size: 70,
+                          ),
+                          Text("Gallery")
+                        ],
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: InkWell(
-                      onTap: () {
-                        _pickImageFromCamera();
-                      },
-                      child: const SizedBox(
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.camera_alt,
-                              size: 70,
-                            ),
-                            Text("Camera")
-                          ],
-                        ),
+                ),
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      _pickImageFromCamera();
+                    },
+                    child: const SizedBox(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.camera_alt,
+                            size: 70,
+                          ),
+                          Text("Camera")
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 
-//Gallery
   Future _pickImageFromGallery() async {
-    final returnImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+    final returnImage = await ImagePicker().pickImage(source: ImageSource.gallery);
     if (returnImage == null) return;
     setState(() {
-      selectedIMage = File(returnImage.path);
+      selectedImage = File(returnImage.path);
       _image = File(returnImage.path).readAsBytesSync();
     });
-    Navigator.of(context).pop(); //close the model sheet
+    Navigator.of(context).pop(); // close the modal sheet
   }
 
-//Camera
   Future _pickImageFromCamera() async {
-    final returnImage =
-        await ImagePicker().pickImage(source: ImageSource.camera);
+    final returnImage = await ImagePicker().pickImage(source: ImageSource.camera);
     if (returnImage == null) return;
     setState(() {
-      selectedIMage = File(returnImage.path);
+      selectedImage = File(returnImage.path);
       _image = File(returnImage.path).readAsBytesSync();
     });
     Navigator.of(context).pop();
   }
 }
+
