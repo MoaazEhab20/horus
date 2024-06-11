56f6d49bdae0f89c846a61b40d1424bffc7dc0df
adham <adhamhamdy682@gmail.com>
2024-06-12 00:49:46 +0300

    credit card

--- lib/screens/touristview/booking_pages/booking_screen.dart
+++ lib/screens/touristview/booking_pages/booking_screen.dart
@@ -1,4 +1,6 @@
+import 'package:final_project/screens/touristview/booking_pages/payment_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../../components/custom_text.dart';
 
@@ -10,6 +12,11 @@ class bookingTourguide extends StatefulWidget {
 }
 
 class _bookingTourguideState extends State<bookingTourguide> {
+  TextEditingController fromTime = TextEditingController();
+  TextEditingController toTime = TextEditingController();
+  TextEditingController location = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,79 +40,140 @@ class _bookingTourguideState extends State<bookingTourguide> {
       ),
       body:  Padding(
         padding: const EdgeInsets.symmetric(horizontal:24.0),
-        child: Column(
-          children: <Widget>[
-            Spacer(flex: 1,),
-            TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                labelText: 'From',labelStyle: TextStyle(color: Theme.of(context).primaryColor),
-                hintText: 'TT',hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: Color(0xffF5903F),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                labelText: 'To',labelStyle: TextStyle(color: Theme.of(context).primaryColor),
-                hintText: 'TT',hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: Color(0xffF5903F),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                labelText: 'Location',labelStyle: TextStyle(color: Theme.of(context).primaryColor),
-                hintText: 'LL',hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: Color(0xffF5903F),
-                  ),
-                ),
-              ),
-            ),
-            Spacer(flex: 1,),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              Spacer(flex: 1,),
+              TextFormField(
+                keyboardType: TextInputType.datetime,
+                textAlign: TextAlign.center,
+                controller: fromTime,
+                validator: (data) {
+                  final format = DateFormat('HH:mm');
+                  DateTime? inputTime;
+                  try {
+                  inputTime = format.parse(data.toString());
+                  } catch (e) {
+                    return '*Invalid time format';
+                    }
+                  final startTime = format.parse('09:00');
+                  final endTime = format.parse('20:00');
+
+                  if (data!.isEmpty) {
+                    return '*Please fill this field';
+                  } else if (inputTime.isBefore(startTime) || inputTime.isAfter(endTime)) {
+                    return '*Time must be between 09:00 and 20:00';
+                    } else {
+                      return null;
+                    }
                 },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  backgroundColor: Color(0xffF5903F),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                ),
-                child: Text(
-                  'CONFIRM',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColor,
+                decoration: InputDecoration(
+                  labelText: 'From',labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+                  hintText: 'TT',hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(
+                      color: Color(0xffF5903F),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Spacer(flex: 2,),
-          ],
+              SizedBox(height: 16.0),
+              TextFormField(
+                keyboardType: TextInputType.datetime,
+                textAlign: TextAlign.center,
+                controller: toTime,
+                validator: (data) {
+                  final format = DateFormat('HH:mm');
+                  DateTime? toInputTime;
+                  DateTime? fromInputTime;
+                  try {
+                  toInputTime = format.parse(data.toString());
+                  fromInputTime = format.parse(fromTime.text.toString());
+                  } catch (e) {
+                    return '*Invalid time format';
+                    }
+                  final startTime = format.parse('10:00');
+                  final endTime = format.parse('21:00');
+
+                  if (data!.isEmpty) {
+                    return '*Please fill this field';
+                  } else if (toInputTime.isBefore(startTime) || toInputTime.isAfter(endTime)) {
+                    return '*Time must be between 10:00 and 21:00';
+                  } else if (toInputTime.isBefore(fromInputTime)) {
+                    return '*Timing error, try another time';
+                  }else if (toInputTime.difference(fromInputTime).inMinutes < 60) {
+                    return '*Your time is less than one hour, try another time';
+                  } else {
+                      return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'To',labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+                  hintText: 'TT',hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(
+                      color: Color(0xffF5903F),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                textAlign: TextAlign.center,
+                controller: location,
+                validator: (data) {
+                  if (data!.isEmpty) {
+                    return '*Please fill this field';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Location',labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+                  hintText: 'LL',hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(
+                      color: Color(0xffF5903F),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(flex: 1,),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Navigator.push(context,MaterialPageRoute(builder: (context) => const creditCard()));
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    backgroundColor: Color(0xffF5903F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  child: Text(
+                    'CONFIRM',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(flex: 2,),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/touristview/booking_pages/payment_screen.dart
+++ lib/screens/touristview/booking_pages/payment_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 import '../../../components/custom_text.dart';
+import '../../../widgets/custom_bottom_bar.dart';
 
-class creditCard extends StatelessWidget {
+class creditCard extends StatefulWidget {
   const creditCard({super.key});
 
+  @override
+  State<creditCard> createState() => _creditCardState();
+}
+
+class _creditCardState extends State<creditCard> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool showBackView = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  void onCreditCardModelChange(CreditCardModel? creditCardModel){
+    setState(() {
+      cardNumber = creditCardModel!.cardNumber;
+      cardHolderName = creditCardModel.cardHolderName;
+      expiryDate = creditCardModel.expiryDate;
+      cvvCode = creditCardModel.cvvCode;
+      showBackView = creditCardModel.isCvvFocused;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +48,76 @@ class creditCard extends StatelessWidget {
         ),
         title: TextForTitleL(data: 'CreditCard'),
       ),
-      body: Column(
-        
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:24.0),
+        child: Column(
+          children: [
+            SizedBox(),
+            CreditCardWidget(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvCode, 
+              showBackView: showBackView, 
+              obscureCardNumber: true,
+              obscureCardCvv: true, 
+              isHolderNameVisible: true,
+              isSwipeGestureEnabled: true,
+              cardType: CardType.mastercard,
+              onCreditCardWidgetChange: (CreditCardBrand ) { },
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      CreditCardForm(
+                        cardNumber: cardNumber,
+                        expiryDate: expiryDate, 
+                        cardHolderName: cardHolderName, 
+                        cvvCode: cvvCode, 
+                        obscureNumber: true,
+                        obscureCvv: true, 
+                        isHolderNameVisible: true,
+                        isExpiryDateVisible: true,
+                        formKey: _formKey,
+                        onCreditCardModelChange: onCreditCardModelChange,
+                        ),
+                        const SizedBox(height: 20,),
+                        SizedBox(
+                        width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  if(_formKey.currentState!.validate()){
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => const CustomBottomBar()));
+                    print('Valid!');
+                  }
+                  else{
+                    print('Invalid!');
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  backgroundColor: Color(0xffF5903F),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+                child: Text(
+                  'Save',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            ),
+                    ],
+                  ),
+                )
+                ),
+          ],
+        ),
       ),
     );
   }
