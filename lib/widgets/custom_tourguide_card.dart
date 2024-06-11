5b7297f5aab32b2700854102e70631b0a2fd2946
adham <adhamhamdy682@gmail.com>
2024-06-11 20:24:16 +0300

    payment

--- lib/widgets/custom_tourguide_card.dart
+++ lib/widgets/custom_tourguide_card.dart
@@ -1,4 +1,4 @@
-import 'package:final_project/screens/touristview/details_tourguide.dart';
+import 'package:final_project/screens/touristview/booking_pages/details_tourguide.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
